eac78f81e9779e19f3a6946015416b243a71fd35
Najma <phnajma786@gmail.com>
2026-04-30 14:48:20 +0530

    response checking2

--- src/controllers/accessCodeController.js
+++ src/controllers/accessCodeController.js
@@ -37,7 +37,7 @@ exports.validateAccessCode = async (req, res) => {
       });
     }
 
-    const existing = await AccessCode.findOne({ code }).lean();
+    const existing = await AccessCode.findOne({ code });
     console.log(existing);
     if (!existing) {
       return res.status(404).json({
